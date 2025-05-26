eac6587c8568e889b06686909ecd29b96ffa78b6
Dat Nguyen <datntdev@outlook.com>
2025-05-27 00:58:10 +0700

    build: gulp tool to preprocess styles and javascripts

---
+++ svcs/datntdev.Microservice.Identity.Web.Host/Themes/tools/gulp.config.js
@@ -0,0 +1,33 @@
+const gulpConfig = {
+	name: "{theme}",
+	desc: "Gulp build config",
+	version: "{version}",
+	config: {
+		debug: false,
+		compile: {
+			jsMinify: false,
+			cssMinify: false,
+			jsSourcemaps: false,
+			cssSourcemaps: false,
+		},
+		path: {
+			src: "../src/{theme}/{demo}",
+			node_modules: "node_modules",
+		},
+		dist: ["../../wwwroot"],
+	},
+	build: {
+		base: {
+			src: {
+				styles: ["{$config.path.src}/sass/style.scss"],
+				scripts: ["{$config.path.src}/js/**/*.js"],
+			},
+			dist: {
+				styles: "{$config.dist}/css/style.css",
+				scripts: "{$config.dist}/js/scripts.js",
+			},
+		},
+	},
+};
+
+export { gulpConfig };
